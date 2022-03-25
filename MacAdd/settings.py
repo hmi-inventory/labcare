4d15c43e2a46a435bd6f7b2ead13954c961b70c5
india-elections <indian.election@outlook.com>
2022-03-25 22:45:27 +0530

    Update settings.py

--- MacAdd/settings.py
+++ MacAdd/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'macaddapp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL=True
 ROOT_URLCONF = 'MacAdd.urls'
 
 TEMPLATES = [
